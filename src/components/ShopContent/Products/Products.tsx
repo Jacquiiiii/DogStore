1cdb8a06ac2a49d19d1cc46c554956ade1c32389
Jacquiiii <jacqui.koroll@telus.com>
2024-04-03 14:41:20 -0700

    feat: add to cart button with onClick dispatch to store

--- src/components/ShopContent/Products/Products.tsx
+++ src/components/ShopContent/Products/Products.tsx
@@ -1,7 +1,11 @@
-import { ProductsProps } from '@/pages/shop'
+import { Product, ProductsProps } from '@/pages/shop'
 import styles from './Products.module.css'
+import { useDispatch } from 'react-redux'
+import { addItemToCart } from '@/store/slices/cartSlice'
 
 const Products = ({ productsData }: ProductsProps) => {
+  const dispatch = useDispatch()
+
   return (
     <div className={styles.products}>
       {productsData.map((product) =>
@@ -10,16 +14,32 @@ const Products = ({ productsData }: ProductsProps) => {
           <div className={styles.productInfo}>
             <h3 className={styles.name}>{product.product_name}</h3>
             <p className={styles.description}>{product.description}</p>
-            <div className={styles.prices}>
+            <div>
               {product.discounted_price !== '0.00' ? 
-                <>
-                  <p className={styles.discountedPrice}>
-                    ${product.discounted_price}
-                  </p>
-                  <p className={styles.strikedPrice}>${product.price}</p>
-                </>
+                <div className={styles.buyContainer}>
+                  <div className={styles.prices}>
+                    <p className={styles.discountedPrice}>
+                      ${product.discounted_price}
+                    </p>
+                    <p className={styles.strikedPrice}>${product.price}</p>
+                  </div>
+                  <button 
+                    className={styles.addToCart} 
+                    onClick={() => dispatch(addItemToCart(product))}
+                  >
+                    Add to cart
+                  </button>
+                </div>
                 :
-                <p className={styles.price}>${product.price}</p>
+                <div className={styles.buyContainer}>
+                  <p className={styles.price}>${product.price}</p>
+                  <button 
+                    className={styles.addToCart} 
+                    onClick={() => dispatch(addItemToCart(product))}
+                  >
+                    Add to cart
+                  </button>
+                </div>
               }
             </div>
           </div>
