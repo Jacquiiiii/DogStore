7fd1ad2772c9a28965aa509c01983daefefb2a72
Jacquiiii <jacqui.koroll@telus.com>
2024-04-22 17:38:20 -0700

    feat: add search bar and update styling

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,13 +1,30 @@
+import { useState, useEffect } from 'react'
 import styles from './Nav.module.css'
 import DesktopNav from './DesktopNav/DesktopNav'
 import MobileNav from './MobileNav/MobileNav'
 
 const Nav = () => {
+  const promos = [
+    'Free shipping on all orders of $50+', 
+    'Buy 10 bags of food and get one free', 
+    'Save 10% on select treats with code SAVE10'
+  ]
+  const [currentPromo, setCurrentPromo] = useState(promos[0])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentPromo(prevPromo => {
+        const nextIndex = (promos.indexOf(prevPromo) + 1) % promos.length
+        return promos[nextIndex]
+      })
+    }, 5000)
+
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <div className={styles.nav}>
-      <div className={styles.promo}>
-        Free shipping on all orders of $50+
-      </div>
+      <div className={styles.promo}>{currentPromo}</div>
       <DesktopNav />
       <MobileNav />
     </div>
