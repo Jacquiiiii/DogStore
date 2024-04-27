13f1fd2e49070f94bd4efd9f8c5f77ea7f10c807
Jacquiiii <jacqui.koroll@telus.com>
2024-04-27 14:29:28 -0700

    fix: update new filter to display 5 newest products

--- src/components/ShopContent/utils/productFilter.ts
+++ src/components/ShopContent/utils/productFilter.ts
@@ -1,19 +1,19 @@
 import { Product } from '@/types/types'
 
 export const productFilter = (productCategory: string, productsData: Product[]) => {
+  // Show any product that has a sales count greater than 50
   if (productCategory === 'bestsellers') {
     return productsData.filter(product => product.sales_count > 50)
   } 
-    
+   
+  // Show the 5 newest products
   if (productCategory === 'new') {
-    return productsData.filter(product => {
-      const productDate = new Date(product.created_at)
-      const oneMonthAgo = new Date()
-      oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1)
-      return productDate >= oneMonthAgo
-    })
+    return productsData
+      .sort((a, b) => new Date(b.created_at).getTime() - new Date(a.created_at).getTime())
+      .slice(0, 5)
   }
   
+  // Show any product that has a discounted price
   if (productCategory === 'deals') {
     return productsData.filter(product => product.discounted_price !== '0.00')
   }
