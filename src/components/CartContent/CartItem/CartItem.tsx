239351c99329ced7cf1847f32d50cdd44c2b4d9c
Jacquiiii <jacqui.koroll@telus.com>
2024-05-15 23:19:07 -0700

    feat: update cart item buttons and calculation

--- src/components/CartContent/CartItem/CartItem.tsx
+++ src/components/CartContent/CartItem/CartItem.tsx
@@ -1,30 +1,35 @@
 import { useDispatch } from 'react-redux'
-import { removeItemFromCart } from '../../../store/slices/cartSlice'
+import { addItemToCart, removeItemFromCart } from '../../../store/slices/cartSlice'
 import styles from './CartItem.module.css'
 import { CartItemProps } from '@/types/types'
+import useCart from '@/hooks/useCart'
 
 const CartItem = ({ product }: CartItemProps) => {
   const dispatch = useDispatch()
-
-  // Convert the price to a fixed decimal number
-  const price = Number(product.price).toFixed(2)
+  const { calculateItemTotal } = useCart()
 
   return (
     <div className={styles.cartItem}>
-      <div className={styles.productName}>
-        <span>{product.product_name}</span>
-        {product.quantity && product.quantity > 1 && 
-          <span className={styles.quantity}>({product.quantity})</span>
-        }
-      </div>
+      <span>{product.product_name}</span>
       <div className={styles.checkout}>
-        <span>${price}</span>
-        <button 
-          onClick={() => dispatch(removeItemFromCart(product))}
-          className={styles.removeButton}
-        >
-          ✖️
-        </button>
+        <span>${calculateItemTotal(product)}</span>
+        <div className={styles.quantityUpdater}>
+          <button 
+            onClick={() => dispatch(removeItemFromCart(product))}
+            className={styles.removeButton}
+          >
+            -
+          </button>
+            <span className={styles.quantity}>
+              {product.quantity}
+            </span>
+          <button 
+            onClick={() => dispatch(addItemToCart(product))}
+            className={styles.addButton}
+          >
+            +
+          </button>
+        </div>
       </div>
     </div>
   )
--- src/hooks/useCart.ts
+++ src/hooks/useCart.ts
@@ -9,16 +9,25 @@ const useCart = () => {
   const initialCartItems = useSelector((state: RootState) => state.cart.cartItems)
   const cartItems: Product[] = []
 
-  // Iterates through state's initialCartItems to check if each item exists in cartItems. If item exists, it increments the quantity and price of the item. Else, it adds the item to cartItems.
+  // Iterates through state's initialCartItems to check if each item exists in cartItems and updates the quantity if so
   for (const item of initialCartItems) {
     const existingItem = cartItems.find((newItem) => newItem.id === item.id)
 
     if (existingItem && existingItem.quantity) {
       existingItem.quantity += 1
-      existingItem.price = (Number(existingItem.price) + Number(item.price)).toString()
     } else {
       cartItems.push({ ...item, quantity: 1 })
     }
+  }
+  
+  // Calculates the total price for one item
+  const calculateItemTotal = (item: Product) => {
+    const quantity = item.quantity || 1
+    const price = item.discounted_price 
+      ? Number(item.discounted_price) 
+      : Number(item.price)
+      
+    return (price * quantity).toFixed(2)
   }
 
   // Calculates the total price of all items in cartItems
@@ -27,14 +36,23 @@ const useCart = () => {
       console.error('Invalid argument: items should be an array')
       return 0
     }
-    const total = items.reduce((acc, item) => acc + Number(item.price), 0)
+
+    const total = items.reduce((acc, item) => {
+      const quantity = item.quantity || 1
+      const price = item.discounted_price 
+        ? Number(item.discounted_price) 
+        : Number(item.price)
+        
+      return acc + (price * quantity)
+    }, 0)
+
     return total.toFixed(2)
   }
 
   // useMemo is used to optimize performance by only recalculating the total when cartItems changes
   const total = useMemo(() => calculateTotal(cartItems), [initialCartItems])
 
-  return { cartItems, total }
+  return { cartItems, total, calculateItemTotal }
 }
 
 export default useCart
