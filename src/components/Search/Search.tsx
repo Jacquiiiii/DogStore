41eac72359f2ae86c07c5b256b4c1cdceafe104c
Jacquiiii <jacqui.koroll@telus.com>
2024-07-05 15:42:50 -0700

    feat: update styling

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,4 +1,4 @@
-import { Raleway } from 'next/font/google'
+import { Open_Sans } from 'next/font/google'
 import { useDispatch } from 'react-redux'
 import { useSelector } from 'react-redux'
 import { useRouter } from 'next/router'
@@ -9,7 +9,7 @@ import { productSearch } from './utils/productSearch'
 import { setProductCategory, setProductSearchMatches } from '@/store/slices/productSlice'
 import { SearchProps } from '@/types/types'
 
-const raleway = Raleway({ subsets: ['latin'] })
+const openSans = Open_Sans({ subsets: ['latin'] })
 
 const Search = ({setMobileIsOpen, mobile}: SearchProps) => {
   const dispatch = useDispatch()
@@ -20,7 +20,7 @@ const Search = ({setMobileIsOpen, mobile}: SearchProps) => {
   return (
     <div className={mobile ? styles.mobileSearch : styles.search}>
       <input 
-        className={`${raleway.className} ${mobile ? styles.mobileSearchInput : styles.searchInput}`} 
+        className={`${openSans.className} ${mobile ? styles.mobileSearchInput : styles.searchInput}`} 
         placeholder='Search' 
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
