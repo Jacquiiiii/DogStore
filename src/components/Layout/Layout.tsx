254dfe9284813050f40bf9edbbd36a9ada7b1a2a
Jacquiiii <jacqui.koroll@telus.com>
2024-07-05 15:42:50 -0700

    feat: update styling

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,16 +1,16 @@
-import { Raleway } from 'next/font/google'
+import { Open_Sans } from 'next/font/google'
 import styles from './Layout.module.css'
 import Footer from '../Footer/Footer'
 import Nav from '../Nav/Nav'
 import ScrollToTop from '../ScrollToTop/ScrollToTop'
 import { LayoutProps } from '@/types/types'
 
-const raleway = Raleway({ subsets: ['latin'] })
+const openSans = Open_Sans({ subsets: ['latin'] })
 
 const Layout = ({ children }: LayoutProps) => {
   return (
     <>
-      <main className={`${raleway.className} ${styles.main}`}>
+      <main className={`${openSans.className} ${styles.main}`}>
         <Nav />
         <div className={styles.topSpacer} />
         {children}
